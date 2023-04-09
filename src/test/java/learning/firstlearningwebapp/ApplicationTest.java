8e6cd3e655053e1ea62039f6f27d204f40882655
fskinner-dev <64882478+fskinner-dev@users.noreply.github.com>
2023-04-09 11:35:48 -0400

    Added project files via upload

---
+++ src/test/java/learning/firstlearningwebapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package learning.firstlearningwebapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
